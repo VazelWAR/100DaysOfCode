031901880eafa0cd21dc8f855ef287ef96cc7ca6
Shashwat Wazalwar <98304037+VazelWAR@users.noreply.github.com>
2024-02-28 09:08:28 +0530

    Add files via upload

---
+++ Day_72/Unique_Binary_Search_Trees_II.py
@@ -0,0 +1,37 @@
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n == 0:
+            return []
+        else:
+            return self.tree_constructor(1, n)
+
+    def tree_constructor(self, m, n):
+        results = []
+        if m > n:
+            results.append(None)
+            return results
+
+        for i in range(m, n+1):
+            l = self.tree_constructor(m, i-1)
+            r = self.tree_constructor(i+1, n)
+            for left_trees in l:
+                for right_trees in r:
+                    curr_node = TreeNode(i)
+                    curr_node.left = left_trees
+                    curr_node.right = right_trees
+                    results.append(curr_node)
+
+
+        return results
